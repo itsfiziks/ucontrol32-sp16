11f90644f60fe5bf2600cb55906510f6643fe924
itsfiziks <gvanhoy@arizona.edu>
2016-01-19 10:25:58 -0700

    main.c

--- main.c
+++ main.c
@@ -1,12 +1,15 @@
 
+#include "arithmetic.h"
+
 
 int main(void){
     
+    int x;
     //initilazation code
     
     // loop
     while(1){
-        
+        x = add(3, 2);
     }
     
     return 0;
