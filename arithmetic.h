7faf297a95f35e3267e398d47cabe4e68b135946
itsfiziks <gvanhoy@arizona.edu>
2016-01-19 10:15:29 -0700

    Now with a new add function. Yay.:

---
+++ arithmetic.h
@@ -0,0 +1,22 @@
+/* 
+ * File:   arithmetic.h
+ * Author: gvanhoy
+ *
+ * Created on January 19, 2016, 10:08 AM
+ */
+
+#ifndef ARITHMETIC_H
+#define	ARITHMETIC_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+void add(int a, int b);
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* ARITHMETIC_H */
+
